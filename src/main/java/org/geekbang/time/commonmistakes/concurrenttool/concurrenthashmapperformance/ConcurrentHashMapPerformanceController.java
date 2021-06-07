3a779a44bba1caebf3c0dd0a174f24f622975009
S.C <ijavacs@gmail.com>
2021-06-07 10:53:52 +0800

    22

--- src/main/java/org/geekbang/time/commonmistakes/concurrenttool/concurrenthashmapperformance/ConcurrentHashMapPerformanceController.java
+++ src/main/java/org/geekbang/time/commonmistakes/concurrenttool/concurrenthashmapperformance/ConcurrentHashMapPerformanceController.java
@@ -24,6 +24,27 @@ public class ConcurrentHashMapPerformanceController {
     private static int LOOP_COUNT = 10000000;
     private static int THREAD_COUNT = 10;
     private static int ITEM_COUNT = 10;
+    
+    /**
+     * 我们可以举一个形象的例子。ConcurrentHashMap 就像是一个大篮子，
+     * 现在这个篮子里有 900 个桔子，我们期望把这个篮子装满 1000 个桔子，也就是再装 100
+     * 个桔子。有 10 个工人来干这件事儿，大家先后到岗后会计算还需要补多少个桔子进去，最
+     * 后把桔子装入篮子。
+     *
+     *ConcurrentHashMap 这个篮子本身，可以确保多个工人在装东西进去时，不会相互影响
+     *干扰，但无法确保工人 A 看到还需要装 100 个桔子但是还未装的时候，工人 B 就看不到篮
+     *子中的桔子数量。更值得注意的是，你往这个篮子装 100 个桔子的操作不是原子性的，在
+     *别人看来可能会有一个瞬间篮子里有 964 个桔子，还需要补 36 个桔子。
+     *
+     * 1. 使用了 ConcurrentHashMap，不代表对它的多个操作之间的状态是一致的，是没有其
+     * 他线程在操作它的，如果需要确保需要手动加锁。
+     * 2. 诸如 size、isEmpty 和 containsValue 等聚合方法，在并发情况下可能会反映
+     * ConcurrentHashMap 的中间状态。因此在并发情况下，这些方法的返回值只能用作参
+     * 考，而不能用于流程控制。显然，利用 size 方法计算差异值，是一个流程控制。
+     * 3. 诸如 putAll 这样的聚合方法也不能确保原子性，在 putAll 的过程中去获取数据可能会
+     * 获取到部分数据。
+     *
+     **/
 
     @GetMapping("good")
     public String good() throws InterruptedException {
